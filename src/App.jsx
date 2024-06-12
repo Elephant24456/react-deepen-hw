a154340ab765861e7690ee71b84809a400275b80
Elephant24456 <rlaxortn0984@naver.com>
2024-06-12 20:00:17 +0900

    add:로그인 페이지, 회원가입 페이지 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import "./App.css";
@@ -77,6 +79,14 @@ function App() {
           <Route
             path="/detail/:id"
             element={<Detail expenses={expenses} setExpenses={setExpenses} />}
+          />
+          <Route
+            path="/sign_in"
+            element={<SignIn />}
+          />
+          <Route
+            path="/sign_up"
+            element={<SignUp />}
           />
         </Routes>
       </BrowserRouter>
